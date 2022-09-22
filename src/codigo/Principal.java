cde44fe4dd6e5c58323f57f81ae9a5089ff6dc96
subAngel <whoangel.agl@gmail.com>
2022-09-22 01:44:10 -0500

    Corregir tres comillas

--- src/codigo/Principal.java
+++ src/codigo/Principal.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package codigo;
 
 import java.io.File;
@@ -11,10 +7,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-/**
- *
- * @author Charly Ponce
- */
 public class Principal {
     public static void main(String[] args) throws Exception {
         String ruta1 = "./src/codigo/Lexer.flex";
--- src/codigo/Tokens.java
+++ src/codigo/Tokens.java
@@ -5,36 +5,23 @@
  */
 package codigo;
 
-/**
- *
- * @author Charly Ponce
- */
 public enum Tokens {
     Linea,
     Comillas,
-    T_dato,
-    Cadena,
+    Int,
+    Character,
     If,
     Else,
-    Do,
     While,
-    For,
     Igual,
     Suma,
     Resta,
     Multiplicacion,
     Division,
-    Op_logico,
-    Op_incremento,
-    Op_relacional,
-    Op_atribucion,
-    Op_booleano,
     Parentesis_a,
     Parentesis_c,
     Llave_a,
     Llave_c,
-    Corchete_a,
-    Corchete_c,
     Main,
     P_coma,
     Identificador,
