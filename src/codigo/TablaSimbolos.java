038e72fb65cd19a82660bd5e652e13b348a9ba7f
subAngel <whoangel.agl@gmail.com>
2022-10-20 16:04:57 -0500

    Add TablaSim.. objeto and modified Sintax.cup

---
+++ src/codigo/TablaSimbolos.java
@@ -0,0 +1,78 @@
+
+package codigo;
+
+import java.util.Vector;
+import codigo.objeto;
+
+public class TablaSimbolos {
+
+    private static Vector tabla = new Vector();
+
+    public TablaSimbolos() {
+    }
+
+    public static void crearEntrada(String nombre, String tipo) {
+        String subStr;
+        objeto e = TablaSimbolos.busca(nombre);
+        if (e == null) {
+            e = new objeto(nombre, tipo);
+            tabla.add(e);
+        } else {
+        }
+    }
+
+    public static String getTipo(String nombre) {
+        objeto e = TablaSimbolos.busca(nombre);
+        if (e == null) {
+
+        }
+        return e.getTipo();
+    }
+
+    public static String getValor(String nombre) {
+        objeto e = TablaSimbolos.busca(nombre);
+        if (e == null) {
+
+        }
+        return e.getValor();
+    }
+
+    public static objeto busca(String nombre) {
+        objeto e = null;
+        int i = 0;
+        while (i < tabla.size()) {
+            e = (objeto) tabla.elementAt(i);
+            if (e.getNombre().equals(nombre)) {
+                break;
+            }
+            e = null;
+            i++;
+        }
+        return e;
+    }
+
+    public static Vector getTabla() {
+        return tabla;
+    }
+
+    public static void Imprimir() {
+        for (int i = 0; i < tabla.size(); i++) {
+            System.out.println(tabla.elementAt(i).toString());
+        }
+    }
+
+    public static void setValor(String pnombre, String pval) {
+        objeto e = null;
+        int i = 0;
+        while (i < tabla.size()) {
+            e = (objeto) tabla.elementAt(i);
+            if (e.getNombre().equals(pnombre)) {
+                e.setValor(pval);
+                tabla.set(i, e);
+                break;
+            }
+            e = null;
+            i++;
+        }
+    }
+}
---
+++ src/codigo/objeto.java
@@ -0,0 +1,43 @@
+
+package codigo;
+
+public class objeto {
+
+    String nombre;
+    String valor;
+    String tipo;
+
+    public String toString() {
+        return nombre + " " + valor + " " + tipo;
+    }
+
+    public objeto(String pnombre, String ptipo) {
+        nombre = pnombre;
+        tipo = ptipo;
+    }
+
+    public void setNombre(String pnombre) {
+        nombre = pnombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setValor(String pvalor) {
+        this.valor = pvalor;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setTipo(String ptipo) {
+        this.tipo = ptipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+}
